7e14f63b36198b4dfc417ed73e47d98f8aca697b
CNGaoWenbo <46814298+CNGaoWenbo@users.noreply.github.com>
2020-09-11 17:21:51 +0800

    Create a136. 只出现一次的数字.java
    异或位运算的性质：
    a异或0 = a 任何数和0异或都是它本身
    a异或a = 0 任何数自己异或自己都是0
    abc=acb 异或满足交换律和结合律

---
+++ 数组/a136. 只出现一次的数字.java
@@ -0,0 +1,30 @@
+/*
+异或位运算的性质：
+a异或0 = a 任何数和0异或都是它本身
+a异或a = 0 任何数自己异或自己都是0
+abc=acb 异或满足交换律和结合律
+
+只需要将所有的数逐个异或一遍，就会剩下单独的数
+
+其他位运算的总结：https://blog.csdn.net/xiaochunyong/article/details/7748713
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for(int num:nums){
+            single ^= num;
+        }
+        return single;
+    }
+}
+/*
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i += 2){
+            if(nums[i] != nums[i+1]) return nums[i];
+        }
+        return nums[nums.length-1];
+    }
+}
+*/
