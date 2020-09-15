ff02e93c1d94025505c34216533043b9dbaaf769
CNGaoWenbo <46814298+CNGaoWenbo@users.noreply.github.com>
2020-09-15 22:51:31 +0800

    Update b69x平方根.java

--- 数学/b69x平方根.java
+++ 数学/b69x平方根.java
@@ -1,4 +1,7 @@
-//字节简单面试题
+/*
+字节简单面试题
+*/
+//二分法
 class Solution {
     public int mySqrt(int x) {
         int left = 0, right = x;
@@ -13,5 +16,19 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+//牛顿迭代法
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        double x0 = x;
+        while(true){
+            double x1 = 0.5*(x0+x/x0);
+            if(x0-x1 < 1e-7) break;
+            x0 = x1;
+        }
+        return (int)x0;
     }
 }
