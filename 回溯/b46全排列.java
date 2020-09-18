378b7d0071c864059d887ecfd17800af56b0f5d9
CNGaoWenbo <46814298+CNGaoWenbo@users.noreply.github.com>
2020-09-18 22:34:43 +0800

    Update b46全排列.java
    res.add(new ArrayList(path));【！注意！】这里的path传递的是path的地址，所以要new ArrayList(path)复制path的值，否则在返回后path清零，只能得一串空列表到[[],[],..]

--- 回溯/b46全排列.java
+++ 回溯/b46全排列.java
@@ -1,4 +1,5 @@
 /*
+看【！注意！】部分
 用数组的顺序存储结果，path[0，ptr]是已经选择好的，path[ptr+1, nums.length-1]是为选择的，swap实际就是确定选择和撤销选择回溯
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
@@ -13,7 +14,7 @@ class Solution {
 
     public void traceBack(int[] nums, List<Integer> path, int ptr){
         if(ptr == nums.length){
-            res.add(new ArrayList(path));
+            res.add(new ArrayList(path));【！注意！】这里的path传递的是path的地址，所以要new ArrayList(path)复制path的值，否则在返回后path清零，只能得一串空列表到[[],[],..]
             return;
         }
         for(int i = ptr; i < nums.length; i ++){
@@ -35,7 +36,7 @@ class Solution {
 
     public void traceBack(int[] nums, List<Integer> path){
         if(path.size() == nums.length){
-            res.add(new ArrayList(path));
+            res.add(new ArrayList(path));//【！注意！】这里的path传递的是path的地址，所以要new ArrayList(path)复制path的值，否则在返回后path清零，只能得一串空列表到[[],[],..]
             return;
         }
         for(int i = 0; i < nums.length; i ++){
@@ -46,3 +47,18 @@ class Solution {
         }
     }
 }
+
+/*
+关于【注意】的测试例子
+class Solution {
+    public void test() {
+    	List<List<Integer>> paths = new ArrayList<>();
+    	List<Integer> path = new ArrayList<>();
+    	for(int i = 0; i < 3; i ++) {
+    		path.add(i);
+    	}
+    	paths.add(path);//直接传递path，此时System.out.println(paths.get(0).toString()) 的结果是[0,1,2]
+    	path.clear();//path清零
+    	System.out.println(paths.get(0).toString());结果是[]
+    }
+}//
