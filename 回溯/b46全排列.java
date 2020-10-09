d2f29ff302335db5ab5dc2f4f0331639932f1219
CNGaoWenbo <46814298+CNGaoWenbo@users.noreply.github.com>
2020-10-09 21:17:26 +0800

    Update b46全排列.java

--- 回溯/b46全排列.java
+++ 回溯/b46全排列.java
@@ -1,8 +1,11 @@
 /*
+做题记录 9.28 10.09
 看【！注意！】部分
-用数组的顺序存储结果，path[0，ptr]是已经选择好的，path[ptr+1, nums.length-1]是为选择的，swap实际就是确定选择和撤销选择回溯
+用数组的顺序存储结果，path[0，ptr]是已经选择好的，path[ptr+1, nums.length-1]是为未选择的，swap实际就是确定选择和撤销选择回溯
+*/
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
+    
     public List<List<Integer>> permute(int[] nums) {
         List<Integer> path = new ArrayList<>();
         for(int num:nums){
@@ -14,7 +17,7 @@ class Solution {
 
     public void traceBack(int[] nums, List<Integer> path, int ptr){
         if(ptr == nums.length){
-            res.add(new ArrayList(path));【！注意！】这里的path传递的是path的地址，所以要new ArrayList(path)复制path的值，否则在返回后path清零，只能得一串空列表到[[],[],..]
+            res.add(new ArrayList(path));//【！注意！】这里的path传递的是path的地址，所以要new ArrayList(path)复制path的值，否则在返回后path清零，只能得一串空列表到[[],[],..]
             return;
         }
         for(int i = ptr; i < nums.length; i ++){
@@ -24,7 +27,7 @@ class Solution {
         }
     }
 }
-*/
+
 //普通的存储方法
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
@@ -48,7 +51,7 @@ class Solution {
     }
 }
 
-/*
+
 关于【注意】的测试例子
 class Solution {
     public void test() {
