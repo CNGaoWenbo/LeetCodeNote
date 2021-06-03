80c201d2ef7f35642c4cd00b7d848fdd2eabe07f
CNGaoWenbo <46814298+CNGaoWenbo@users.noreply.github.com>
2021-06-03 15:15:01 +0800

    Update a.79单词搜索.java

--- DFS,BFS/a.79单词搜索.java
+++ DFS,BFS/a.79单词搜索.java
@@ -1,21 +1,13 @@
-/*
-DFS
-递归写的仍不熟练
-
-几个暂时没想明白的问题：
-为什么要传递vis给check()?
-vis的变化，为什么vis最后要重新置false?
-*/
 class Solution {
+    int[] dx = new int[]{0, 0, 1, -1};
+    int[] dy = new int[]{1,-1, 0, 0};
     public boolean exist(char[][] board, String word) {
-        if(word.length() == 0) return true;
-        if(board.length == 0) return false;
-        int rowLen = board.length;
-        int colLen = board[0].length;
-        boolean[][] vis = new boolean[rowLen][colLen];
-        for(int i = 0; i < rowLen; i ++){
-            for(int j = 0; j < colLen; j ++){
-                if(check(board, vis, i, j, word, 0)){
+        int m = board.length;
+        if(m == 0) return false;
+        int n = board[0].length;
+        for(int i = 0; i < m; i ++){
+            for(int j = 0; j < n; j ++){
+                if(dfs(board, i, j, word, 0)){
                     return true;
                 }
             }
@@ -23,27 +15,21 @@ class Solution {
         return false;
     }
 
-    public boolean check(char[][] board, boolean[][] vis, int row, int col, String word, int k){
-        int rowLen = board.length;
-        int colLen = board[0].length;      
-        char target = word.charAt(k);
-        if(board[row][col] != target) return false;
-        else if(k == word.length()-1) return true;
-
-        int[] dirX = new int[]{0, 0, 1, -1};
-        int[] dirY = new int[]{1, -1, 0, 0};
-
-        vis[row][col] = true;
-        for(int index = 0; index < 4; index ++){
-            int newRow = row+dirX[index];
-            int newCol = col+dirY[index];
-            if(newRow >= 0 && newRow < rowLen && newCol >= 0 && newCol < colLen && !vis[newRow][newCol]){
-                if(check(board, vis, newRow, newCol, word, k+1)){
-                    return true;                    
-                }
-            } 
+    public boolean dfs(char[][] board, int x, int y, String word, int u){
+        if(board[x][y] != word.charAt(u)) return false;
+        if(u == word.length()-1) return true;
+        
+        board[x][y] = ' ';
+        for(int i = 0; i < 4; i ++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length){
+                if(dfs(board, newX, newY, word, u+1)){
+                    return true;
+                } 
+            }
         }
-        vis[row][col] = false;
+        board[x][y] = word.charAt(u);
         return false;
     }
 }
